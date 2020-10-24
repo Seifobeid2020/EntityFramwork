4af29dcd8ee0e4f7c1f57dc06b4bf600e3985b43
seifobeid2020 <201706483@bethlehem.edu>
2020-10-24 11:06:49 +0300

    Add project files.

---
+++ Models/Tasks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFramwork.Models
+{
+    public partial class Tasks
+    {
+        public int TaskId { get; set; }
+        public string Subject { get; set; }
+        public DateTime? CreateTime { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? DueDate { get; set; }
+        public DateTime? CompletedDate { get; set; }
+        public bool Complete { get; set; }
+        public string Importance { get; set; }
+    }
+}
